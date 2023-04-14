f4b8d0cc2dcee4958786d7d81a9230f36d95dc18
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-04-14 10:09:21 -0300

    Fixed dotenv

--- server.js
+++ server.js
@@ -3,20 +3,23 @@
  *
  * Uses sqlite.js to connect to db
  */
+require('dotenv').config()
+
+const db = require("./sqlite.js");
 
 const fastify = require("fastify")({
   // Set this to true for detailed logging:
   logger: false
 });
 
+const routes = { endpoints: [] };
+
+const errorMessage =
+  "Whoops! Error connecting to the database, please try again!";
+
 fastify.register(require("@fastify/formbody"));
 
-const db = require("./sqlite.js");
-const errorMessage =
-  "Whoops! Error connecting to the database–please try again!";
-
 // OnRoute hook to list endpoints
-const routes = { endpoints: [] };
 fastify.addHook("onRoute", routeOptions => {
   routes.endpoints.push(routeOptions.method + " " + routeOptions.path);
 });
@@ -35,7 +38,7 @@ fastify.get("/messages", async (request, reply) => {
   let data = {};
   data.chat = await db.getMessages();
   console.log(data.chat);
-  if(!data.chat) data.error = errorMessage;
+  if (!data.chat) data.error = errorMessage;
   const status = data.error ? 400 : 200;
   reply.status(status).send(data);
 });
@@ -44,18 +47,18 @@ fastify.get("/messages", async (request, reply) => {
 fastify.post("/message", async (request, reply) => {
   let data = {};
   const auth = authorized(request.headers.admin_key);
-  if(!auth || !request.body || !request.body.message) data.success = false;
-  else if(auth) data.success = await db.addMessage(request.body.message);
+  if (!auth || !request.body || !request.body.message) data.success = false;
+  else if (auth) data.success = await db.addMessage(request.body.message);
   const status = data.success ? 201 : auth ? 400 : 401;
   reply.status(status).send(data);
 });
 
 // Update text for an message (auth)
-fastify.put("/message", async (request, reply) => { 
+fastify.put("/message", async (request, reply) => {
   let data = {};
   const auth = authorized(request.headers.admin_key);
-  if(!auth || !request.body || !request.body.id || !request.body.message) data.success = false;
-  else data.success = await db.updateMessage(request.body.id, request.body.message); 
+  if (!auth || !request.body || !request.body.id || !request.body.message) data.success = false;
+  else data.success = await db.updateMessage(request.body.id, request.body.message);
   const status = data.success ? 201 : auth ? 400 : 401;
   reply.status(status).send(data);
 });
@@ -64,7 +67,7 @@ fastify.put("/message", async (request, reply) => {
 fastify.delete("/message", async (request, reply) => {
   let data = {};
   const auth = authorized(request.headers.admin_key);
-  if(!auth || !request.query || !request.query.id) data.success = false;
+  if (!auth || !request.query || !request.query.id) data.success = false;
   else data.success = await db.deleteMessage(request.query.id);
   const status = data.success ? 201 : auth ? 400 : 401;
   reply.status(status).send(data);
@@ -83,7 +86,7 @@ const authorized = key => {
 };
 
 // Run the server and report out to the logs
-fastify.listen({port:process.env.PORT, host:'0.0.0.0'}, function(err, address) {
+fastify.listen({ port: process.env.PORT, host: '0.0.0.0' }, function (err, address) {
   if (err) {
     console.error(err);
     process.exit(1);
