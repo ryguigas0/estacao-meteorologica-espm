5818f869dae3a624d3e1b9b5f70ac1d3b2d4a6d0
VictorKMachado <victorfogarel@gmail.com>
2023-05-11 14:53:38 -0300

    Update estacao_metereologica.ino

--- arduino/estacao_metereologica.ino
+++ arduino/estacao_metereologica.ino
@@ -1,88 +1,70 @@
-#include <DHT.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include<ESP8266WiFi.h> //Biblioteca para funcionamento do WiFi do ESP
+#include<ESP8266WebServer.h> //Biblioteca para o ESP funcionar como servidor
+#include <DHT.h> //Biblioteca para funcionamento do sensor de temperatura e umidade DHT11
 
-float temp, umid;
-int pino_dht = 9;
-DHT dht(pino_dht, DHT11);
+const char* ssid = "sua_rede_wifi";  // Rede WiFi
+const char* password = "senha_do_seu_wifi";  //Senha da Rede WiFi
+
+
+#define DHTPIN D7 //Pino digital D2 ao DHT11
+#define DHTTYPE DHT11 //Tipo do sensor DHT11
+DHT dht(DHTPIN, DHTTYPE); 
+
+float temperatura; 
+float umidade; 
+
+
 
 void setup() {
-  Serial.begin(9600);
-  dht.begin();
-}
+  Serial.begin(9600); //Inicializa a comunicação serial
+  delay(50); // Intervalo para aguardar a estabilização do sistema
+  dht.begin(); //Inicializa o sensor DHT11
 
-void loop() {
-  delay(2000);
-  temp = dht.readTemperature();
-  umid = dht.readHumidity();
+  Serial.println("Conectando a Rede: "); //Imprime na serial a mensagem
+  Serial.println(ssid); //Imprime na serial o nome da Rede Wi-Fi
 
-  Serial.print("TEMP: ");
-  Serial.println(temp);
-  Serial.print("UMID: ");
-  Serial.println(umid);
-}
+  WiFi.begin(ssid, password); //Inicialização da comunicação Wi-Fi
 
-
-
-const char* ssid = "colocar_o_SSID";
-const char* password = "colocar_a_senha";
-
-//Your Domain name with URL path or IP address with path
-const char* serverName = "https://estacao-metereologia.glitch.me";
-unsigned long lastTime = 0;
-unsigned long timerDelay = 5000;
-
-void setup() {
-  Serial.begin(115200);
-
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  //Verificação da conexão
+  while (WiFi.status() != WL_CONNECTED) { //Enquanto estiver aguardando status da conexão
+    delay(1000);
+    Serial.print("."); //Imprime pontos
   }
   Serial.println("");
-  Serial.print("Conectado com o WiFi e IP: ");
-  Serial.println(WiFi.localIP());
- 
-  Serial.println("timer de 5 segundos (timerDelay variable), vai levar 5 segundos.");
+  Serial.println("WiFi Conectado");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP()); //Função para exibir o IP da ESP
+
+  server.on("/", handle_OnConnect); //Servidor recebe uma solicitação HTTP - chama a função handle_OnConnect
+  server.onNotFound(handle_NotFound); //Servidor recebe uma solicitação HTTP não especificada - chama a função handle_NotFound
+
+  //server.begin(); //Inicializa o servidor
+  //Serial.println("Servidor HTTP inicializado");
+
 }
 
 void loop() {
-  //Send an HTTP POST request every 10 minutes
-  if ((millis() - lastTime) > timerDelay) {
-    //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
-    
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, serverName);
-      
-      // If you need Node-RED/server authentication, insert user and password below
-      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
-      
-      // Specify content-type header
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      // Data to send with HTTP POST
-      String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&value1=24.25&value2=49.54&value3=1005.14";           
-      // Send HTTP POST request
-      int httpResponseCode = http.POST(httpRequestData);
-      
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-        
-      // Free resources
-      http.end();
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
-  }
+
+
+   server.handleClient(); //Chama o método handleClient() para lidar com as solicitações do cliente
 }
-http.setAuthorization("https://estacao-metereologia.glitch.me", "GeriatricFilling");
-POST /update-sensor HTTP/1.1
-Host: example.com
-{api_key: "tPmAT5Ab3j7F9", sensor_name: "dht", temperature: temp; humidity: umid; tempo: __TIME__}
-Content-Type: application/json
+
+void handle_OnConnect() {
+  temperatura = dht.readTemperature();  //Realiza a leitura da temperatura
+  umidade = dht.readHumidity(); //Realiza a leitura da umidade
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura); //Imprime no monitor serial o valor da temperatura lida
+  Serial.println(" ºC");
+  Serial.print("Umidade: ");
+  Serial.print(umidade); //Imprime no monitor serial o valor da umidade lida
+  Serial.println(" %");
+  server.send(200, "text/html", montarHTML(temperatura, umidade)); //Envia as informações usando o código 200, especifica o conteúdo como "text/html" e chama a função montarHTML
+
+}
+
+void handle_NotFound() { //Função para lidar com o erro 404
+  server.send(404, "text/plain", "Não encontrado"); //Envia o código 404, especifica o conteúdo como "text/pain" e envia a mensagem "Não encontrado"
+
+}
+
+
